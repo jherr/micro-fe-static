cde824afae1ec0a9f5fd9d35d2520867924c015e
Herrington, Jack <Jack.Herrington@nike.com>
2019-12-19 09:27:05 -0800

    Updates

--- page.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-document.getElementById('buy').addEventListener('click', () => {
-  count += 1;
-  window.dispatchEvent(new CustomEvent('updateCart', {
-    detail: {
-      count,
-    }
-  }))
-});
